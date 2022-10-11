394ed664998e56406399b896372554f59966f045
Israel Forés <israel.fores@ticxcat.cat>
2022-10-11 11:24:43 +0200

    Add cron.php for cleaning sessions

---
+++ cron.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once('configuration.php');
+
+$CONF = new Jconfig();
+
+$token = isset($_GET['token']) ? $_GET['token'] : false;
+if ($token == $CONF->tokenCron) {
+    $content = shell_exec('/usr/bin/php7.4 /dades/html/educacio/cli/sessionMetadataGc.php');
+    echo 'OK cron Portal';
+} else {
+    echo '<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8" /></head><body>';
+    echo "Acc&eacute;s incorrecte.<br />";
+    echo '</body></html>';
+}
+~   
